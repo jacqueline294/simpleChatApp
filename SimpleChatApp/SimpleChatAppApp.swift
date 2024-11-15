d60fb0922159456ded9e73b470e9154b0c706396
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-11-15 14:12:23 +0100

    Initial Commit

---
+++ SimpleChatApp/SimpleChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChatAppApp.swift
+//  SimpleChatApp
+//
+//  Created by jacqueline Ngigi on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
