0eb711114772c1fffef172998711f657a9b728fb
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-11-15 14:45:19 +0100

    addinf firebase

---
+++ SimpleChatApp/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  SimpleChatApp
+//
+//  Created by jacqueline Ngigi on 2024-11-15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
